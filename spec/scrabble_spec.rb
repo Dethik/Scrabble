01570954bbcee999dfd14d7a1539d6cbbc1feaae
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-23 11:11:33 -0800

    add good fail for second test
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -6,4 +6,8 @@ describe('Word#scrabble') do
     word = Word.new("a")
     expect(word.scrabble()).to(eq(1))
   end
+  it("returns a scrabble score for a letter using our points hash") do
+    word = Word.new("b")
+    expect(word.scrabble()).to(eq(3))
+  end
 end
