5ac88471ab149c0498b5f34c4bce3377cca4925d
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-02-23 11:20:10 -0800

    pass second test
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -3,10 +3,22 @@ class Word
     @word = str
   end
   def scrabble()
+    points_table = {
+      1 => ["a", "e", "i", "o", "u", "l", "r", "s", "t"],
+      2 => ["d", "g"],
+      3 => ["b", "c", "m", "p"],
+      4 => ["f", "h", "v", "w", "y"],
+      5 => ["k"],
+      8 => ["j", "x"],
+      10 => ["q", "z"]}
     letters = @word.split("")
     letters.each do |letter|
-      if (letter == "a")
+      if ( points_table[1].include?(letter))
         return 1
+      elsif ( points_table[2].include?(letter))
+        return 2
+      elsif ( points_table[3].include?(letter))
+        return 3
       end
     end
   end
